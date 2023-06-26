a1fcb836dfa20620e23745a1cd17740638ccb58b
hipolito <huaylla7@hotmail.com>
2023-06-26 13:08:34 -0400

    Binding habilitado y iniciado

--- app/src/main/java/org/matematicasfacil/disenobotones/MainActivity.java
+++ app/src/main/java/org/matematicasfacil/disenobotones/MainActivity.java
@@ -3,12 +3,17 @@ package org.matematicasfacil.disenobotones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import org.matematicasfacil.disenobotones.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
