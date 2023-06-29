c6a6d47f811560f92a5609bf9e61a9c8ef9fd007
hipolito <huaylla7@hotmail.com>
2023-06-28 20:54:43 -0400

    Individual 11 terminado

--- app/src/main/java/org/matematicasfacil/disenobotones/MainActivity.java
+++ app/src/main/java/org/matematicasfacil/disenobotones/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-
+import com.google.android.material.snackbar.Snackbar;
 import org.matematicasfacil.disenobotones.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-        setContentView(view);
+        setContentView(binding.getRoot());
+
+        binding.btnAvion.setOnClickListener(v -> Snackbar.make(view, "Modo avión Activado", Snackbar.LENGTH_LONG).show());
+        binding.btnGaleria.setOnClickListener(v -> Snackbar.make(view, "Imagen guardada en galería", Snackbar.LENGTH_LONG).show());
+        binding.btnCompletado.setOnClickListener(v -> Snackbar.make(view, "Nivel 4 completado", Snackbar.LENGTH_LONG).show());
+        binding.btnPerfil.setOnClickListener(v -> Snackbar.make(view, "Actualizar perfi", Snackbar.LENGTH_LONG).show());
+        binding.btnDesbloqueo.setOnClickListener(v -> Snackbar.make(view, "Nivel 5 desbloqueado", Snackbar.LENGTH_LONG).show());
+        binding.btnMusica.setOnClickListener(v -> Snackbar.make(view,"Música pausada", Snackbar.LENGTH_LONG).show());
     }
 }
